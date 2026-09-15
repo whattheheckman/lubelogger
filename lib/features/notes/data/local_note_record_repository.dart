fc1acdd27e7ba27ea976da8c3adb9c2015d909dd
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2026-09-15 12:27:32 -0700

    remove redundant imports

--- lib/features/notes/data/local_note_record_repository.dart
+++ lib/features/notes/data/local_note_record_repository.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:drift/drift.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'package:lubelogger/core/database/app_database.dart';
